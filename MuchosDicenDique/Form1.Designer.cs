188b7a764669a6bc2b8d32455387f0e57a184c83
Just A Green Toast <42865757+PabloFon03@users.noreply.github.com>
2023-03-10 13:48:32 +0100

    finiquitao

--- MuchosDicenDique/Form1.Designer.cs
+++ MuchosDicenDique/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace MuchosDicenDique
             this.label1 = new System.Windows.Forms.Label();
             this.Label_IPHost = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label11 = new System.Windows.Forms.Label();
             this.Label_VBVersion = new System.Windows.Forms.Label();
             this.Label_InternetStatus = new System.Windows.Forms.Label();
             this.Label_SSIDStatus = new System.Windows.Forms.Label();
@@ -55,16 +56,18 @@ namespace MuchosDicenDique
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(5, 0);
+            this.label1.Font = new System.Drawing.Font("VCR OSD Mono", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(100, 20);
             this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 25);
+            this.label1.Size = new System.Drawing.Size(1000, 96);
             this.label1.TabIndex = 0;
-            this.label1.Text = "IP Host:";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Text = "Welcome to VirtualBox Management";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Label_IPHost
             // 
+            this.Label_IPHost.Font = new System.Drawing.Font("VCR OSD Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.Label_IPHost.Location = new System.Drawing.Point(125, 0);
             this.Label_IPHost.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.Label_IPHost.Name = "Label_IPHost";
@@ -86,6 +89,7 @@ namespace MuchosDicenDique
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Controls.Add(this.label11, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.Label_VBVersion, 9, 1);
             this.tableLayoutPanel1.Controls.Add(this.Label_InternetStatus, 7, 1);
             this.tableLayoutPanel1.Controls.Add(this.Label_SSIDStatus, 5, 1);
@@ -93,7 +97,6 @@ namespace MuchosDicenDique
             this.tableLayoutPanel1.Controls.Add(this.label6, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.label2, 2, 0);
             this.tableLayoutPanel1.Controls.Add(this.Label_Username, 3, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.Label_IPHost, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.label3, 4, 0);
             this.tableLayoutPanel1.Controls.Add(this.label4, 6, 0);
@@ -106,7 +109,8 @@ namespace MuchosDicenDique
             this.tableLayoutPanel1.Controls.Add(this.Label_MAC, 7, 0);
             this.tableLayoutPanel1.Controls.Add(this.Label_VBInstalled, 9, 0);
             this.tableLayoutPanel1.Controls.Add(this.Label_IPGateway, 1, 1);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(15, 80);
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("VCR OSD Mono", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(20, 141);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
@@ -114,8 +118,20 @@ namespace MuchosDicenDique
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1200, 50);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("VCR OSD Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(5, 0);
+            this.label11.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(110, 25);
+            this.label11.TabIndex = 2;
+            this.label11.Text = "IP Host:";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
             // Label_VBVersion
             // 
+            this.Label_VBVersion.Font = new System.Drawing.Font("VCR OSD Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.Label_VBVersion.Location = new System.Drawing.Point(1085, 25);
             this.Label_VBVersion.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.Label_VBVersion.Name = "Label_VBVersion";
@@ -126,6 +142,7 @@ namespace MuchosDicenDique
             // 
             // Label_InternetStatus
             // 
+            this.Label_InternetStatus.Font = new System.Drawing.Font("VCR OSD Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.Label_InternetStatus.Location = new System.Drawing.Point(845, 25);
             this.Label_InternetStatus.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.Label_InternetStatus.Name = "Label_InternetStatus";
@@ -136,6 +153,7 @@ namespace MuchosDicenDique
             // 
             // Label_SSIDStatus
             // 
+            this.Label_SSIDStatus.Font = new System.Drawing.Font("VCR OSD Mono", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.Label_SSIDStatus.Location = new System.Drawing.Point(605, 25);
             this.Label_SSIDStatus.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.Label_SSIDStatus.Name = "Label_SSIDStatus";
@@ -146,7 +164,7 @@ namespace MuchosDicenDique
             // 
             // Label_Hostname
             // 
-            this.Label_Hostname.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Hostname.Font = new System.Drawing.Font("VCR OSD Mono", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.Label_Hostname.Location = new System.Drawing.Point(365, 25);
             this.Label_Hostname.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.Label_Hostname.Name = "Label_Hostname";
@@ -157,6 +175,7 @@ namespace MuchosDicenDique
             // 
             // label6
             // 
+            this.label6.Font = new System.Drawing.Font("VCR OSD Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(5, 25);
             this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label6.Name = "label6";
@@ -167,6 +186,7 @@ namespace MuchosDicenDique
             // 
             // label2
             // 
+            this.label2.Font = new System.Drawing.Font("VCR OSD Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(245, 0);
             this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label2.Name = "label2";
@@ -177,6 +197,7 @@ namespace MuchosDicenDique
             // 
             // Label_Username
             // 
+            this.Label_Username.Font = new System.Drawing.Font("VCR OSD Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.Label_Username.Location = new System.Drawing.Point(365, 0);
             this.Label_Username.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.Label_Username.Name = "Label_Username";
@@ -187,6 +208,7 @@ namespace MuchosDicenDique
             // 
             // label3
             // 
+            this.label3.Font = new System.Drawing.Font("VCR OSD Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(485, 0);
             this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label3.Name = "label3";
@@ -197,7 +219,7 @@ namespace MuchosDicenDique
             // 
             // label4
             // 
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.label4.Font = new System.Drawing.Font("VCR OSD Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(725, 0);
             this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label4.Name = "label4";
@@ -208,6 +230,7 @@ namespace MuchosDicenDique
             // 
             // label5
             // 
+            this.label5.Font = new System.Drawing.Font("VCR OSD Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(965, 0);
             this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label5.Name = "label5";
@@ -218,6 +241,7 @@ namespace MuchosDicenDique
             // 
             // label7
             // 
+            this.label7.Font = new System.Drawing.Font("VCR OSD Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(245, 25);
             this.label7.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label7.Name = "label7";
@@ -228,6 +252,7 @@ namespace MuchosDicenDique
             // 
             // label8
             // 
+            this.label8.Font = new System.Drawing.Font("VCR OSD Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(485, 25);
             this.label8.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label8.Name = "label8";
@@ -238,6 +263,7 @@ namespace MuchosDicenDique
             // 
             // label9
             // 
+            this.label9.Font = new System.Drawing.Font("VCR OSD Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.label9.Location = new System.Drawing.Point(725, 25);
             this.label9.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label9.Name = "label9";
@@ -248,7 +274,7 @@ namespace MuchosDicenDique
             // 
             // label10
             // 
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Font = new System.Drawing.Font("VCR OSD Mono", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.label10.Location = new System.Drawing.Point(965, 25);
             this.label10.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label10.Name = "label10";
@@ -259,7 +285,7 @@ namespace MuchosDicenDique
             // 
             // Label_SSIDName
             // 
-            this.Label_SSIDName.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_SSIDName.Font = new System.Drawing.Font("VCR OSD Mono", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.Label_SSIDName.Location = new System.Drawing.Point(605, 0);
             this.Label_SSIDName.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.Label_SSIDName.Name = "Label_SSIDName";
@@ -270,6 +296,7 @@ namespace MuchosDicenDique
             // 
             // Label_MAC
             // 
+            this.Label_MAC.Font = new System.Drawing.Font("VCR OSD Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.Label_MAC.Location = new System.Drawing.Point(845, 0);
             this.Label_MAC.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.Label_MAC.Name = "Label_MAC";
@@ -280,6 +307,7 @@ namespace MuchosDicenDique
             // 
             // Label_VBInstalled
             // 
+            this.Label_VBInstalled.Font = new System.Drawing.Font("VCR OSD Mono", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.Label_VBInstalled.Location = new System.Drawing.Point(1085, 0);
             this.Label_VBInstalled.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.Label_VBInstalled.Name = "Label_VBInstalled";
@@ -290,6 +318,7 @@ namespace MuchosDicenDique
             // 
             // Label_IPGateway
             // 
+            this.Label_IPGateway.Font = new System.Drawing.Font("VCR OSD Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.Label_IPGateway.Location = new System.Drawing.Point(125, 25);
             this.Label_IPGateway.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.Label_IPGateway.Name = "Label_IPGateway";
@@ -303,11 +332,14 @@ namespace MuchosDicenDique
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.ClientSize = new System.Drawing.Size(1232, 203);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VirtualBox Manager";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -336,5 +368,6 @@ namespace MuchosDicenDique
         private System.Windows.Forms.Label Label_MAC;
         private System.Windows.Forms.Label Label_VBInstalled;
         private System.Windows.Forms.Label Label_IPGateway;
+        private System.Windows.Forms.Label label11;
     }
 }
